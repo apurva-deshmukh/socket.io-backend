d8cd5996b249c42b4a346d772edf1a8cd6dcae57
apurva-deshmukh <aadeshmukh13@gmail.com>
2020-08-01 17:00:55 -0400

    unique id for connected devices and broadcast messages

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
 var io = require("socket.io")();
 
+let currentUserId = 2;
+const userIds = {};
+let currentMessageId = 1;
+
+function createMessage(userId, messageText) {
+  return {
+    _id: currentMessageId++,
+    text: messageText,
+    createdAt: new Date(),
+    user: {
+      _id: userId,
+      name: "Test User",
+      avatar: "https://placeimg.com/140/140/any",
+    },
+  };
+}
+
 io.on("connection", (socket) => {
   console.log("connected");
-  socket.on("message", (message) => {
+  console.log(socket.id);
+  userIds[socket.id] = currentUserId++;
+  socket.on("message", (messageText) => {
+    const userId = userIds[socket.id];
+    const message = createMessage(userId, messageText);
     console.log(message);
+    socket.broadcast.emit("message", message);
   });
 });
 
